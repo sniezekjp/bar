11505681d2398c13e4f8266924d940c8879b1c22
JP <jsniezek@crowdskout.com>
2015-03-18 11:43:32 -0400

    Removed deps out of bootstrap

--- es6/app/bootstrap.js
+++ es6/app/bootstrap.js
@@ -2,20 +2,6 @@ import router from 'router';
 import lazy from 'lazy';
 import angular from 'angular';
 import components from '../components/components';
-import dashboard from 'modules/dashboard/states';
-import account from 'modules/account/states';
-import main from 'modules/main/states';
-import calllog from 'modules/calllog/states';
-import bowl from 'modules/bowl/states';
-import autocomplete from 'modules/autocomplete/states';
-import input from 'modules/input/states';
-import chair from 'modules/chair/states';
-import table from 'modules/table/states';
-import sidebar from 'modules/sidebar/states';
-import footer from 'modules/footer/states';
-import bigButton from 'modules/bigButton/states';
-import modal from 'modules/modal/states';
-import roadshow from 'modules/roadshow/states';
 /// dependencies (do not remove)
 
 angular.element(document).ready(function() {
