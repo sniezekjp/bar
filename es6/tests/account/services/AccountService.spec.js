6b1374cdf39f8e508d03af1777db1303572482ed
JP <jsniezek@crowdskout.com>
2015-03-18 11:28:12 -0400

    Removed mock stuff

--- es6/tests/account/services/AccountService.spec.js
+++
@@ -1,8 +0,0 @@
-import mocks from 'mocks';
-import AccountService from 'modules/account/services/AccountService';
-
-describe('AccountService', function() {
-  it('should work', function() {
-    expect(true).toBe(true);
-  });
-});
