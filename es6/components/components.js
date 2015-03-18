cb7862c4c551309b05dcc4be77567652020f145d
JP <jsniezek@crowdskout.com>
2015-03-18 11:29:02 -0400

    Removed more mock stuff

--- es6/components/components.js
+++ es6/components/components.js
@@ -1,5 +1,1 @@
-import modal from './modal/modal';
-import sidebar from './sidebar/sidebar';
-import header from './header/header';
-import footer from './footer/footer';
 /// dependencies (do not remove)
--- es6/modules/main/states.js
+++
@@ -1,3 +0,0 @@
-import app from 'app';
-import moduleMenu from './directives/moduleMenu';
-/// dependencies
